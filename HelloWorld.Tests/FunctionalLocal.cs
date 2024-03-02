c97f2ca795678174b4160f9b818dcd6ee853a03a
EliasDeHondt <Elias.dehondt@outlook.com>
2024-03-02 14:08:51 +0100

    Update project files and remove unnecessary comments

--- HelloWorld.Tests/FunctionalLocal.cs
+++ HelloWorld.Tests/FunctionalLocal.cs
@@ -1,27 +1,12 @@
-/*
- * Copyright (c) 2023 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
+/***************************************
+ *                                     *
+ *   Created by Elias De Hondt         *
+ *   Visit https://eliasdh.com         *
+ *                                     *
+ ***************************************/
 
 using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
-
-/// <summary>
-/// Provides simple tests to demonstrate calling the sample app. This is
-/// based on the straightforward examples provided by Microsoft here:
-/// https://learn.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-6.0
-/// </summary>
 public class FunctionalTests : IClassFixture<WebApplicationFactory<Program>>
 {
     private readonly WebApplicationFactory<Program> _factory;
@@ -41,4 +26,3 @@ public class FunctionalTests : IClassFixture<WebApplicationFactory<Program>>
 	Assert.Equal("Hello World!", body);
     }
 }
-
